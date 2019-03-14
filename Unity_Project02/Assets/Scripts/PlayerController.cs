c13cbd5724599b21419e89d57ef8bec07d56b126
Quinn K Wolter <QuinnKWolter@gmail.com>
2019-03-13 21:45:24 -0400

    Initial Commit + Security Cameras!

---
+++ Unity_Project02/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	CharacterController player;
+
+	public float moveSpeed;
+	public float jumpSpeed;
+
+	// Simple Mouse Look Script
+	// https://answers.unity.com/questions/29741/mouse-look-script.html
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	float rotationX = 0F;
+	float rotationY = 0F;
+	Quaternion originalRotation;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<CharacterController>();
+		// Make the rigid body not change rotation
+		if (GetComponent<Rigidbody>())
+			GetComponent<Rigidbody>().freezeRotation = true;
+		originalRotation = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.isGrounded && Input.GetButton("Jump")){
+			player.Move(new Vector3(0,jumpSpeed,0) * Time.deltaTime);
+		}
+		Vector3 frontBack = Input.GetAxis("Vertical")*transform.TransformDirection(Vector3.forward)*moveSpeed;
+		Vector3 leftRight = Input.GetAxis("Horizontal")*transform.TransformDirection(Vector3.right)*moveSpeed;
+		player.Move((frontBack + leftRight) * Time.deltaTime);
+		player.SimpleMove(Physics.gravity);
+
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			// Read the mouse input axis
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+			Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, -Vector3.right);
+			transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+			rotationX = ClampAngle (rotationX, minimumX, maximumX);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+			transform.localRotation = originalRotation * xQuaternion;
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = ClampAngle (rotationY, minimumY, maximumY);
+			Quaternion yQuaternion = Quaternion.AngleAxis (-rotationY, Vector3.right);
+			transform.localRotation = originalRotation * yQuaternion;
+		}
+	}
+
+	public static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360F) {
+			angle += 360F;
+		}
+		if (angle > 360F) {
+			angle -= 360F;
+		}
+		return Mathf.Clamp (angle, min, max);
+	}
+
+	void OnTriggerExit(Collider col) {
+		Debug.Log(col);
+		if (col.tag == "CameraInfluence") {
+			Debug.Log("Escaped!");
+			col.gameObject.transform.parent.GetComponent<SecurityCam>().AlertCamera(false);
+		}
+	}
+}
---
+++ Unity_Project02/Assets/SecurityCam.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCam : MonoBehaviour {
+
+	public GameObject player, neck, head, eye, influence;
+	public GameObject[] sightTargets;
+	public AudioSource alert, alarm;
+	public float rotationSpeed, maxRotation;
+
+	private bool alerted = false;
+	private Quaternion headStartRotation;
+
+	// Use this for initialization
+	void Start () {
+		headStartRotation = head.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// When player exits a trigger, if trigger object is camera, set alerted to false for that camera
+
+	void FixedUpdate() {
+		RaycastHit hit;
+		if (!alerted) {
+			alarm.Stop();
+			neck.transform.rotation = Quaternion.Euler(0f, maxRotation * Mathf.Sin(Time.time * rotationSpeed), 0f);
+			for (int i=0; i<4; i++) {
+				// Unity Documentation
+				if (Physics.Raycast(eye.transform.position, sightTargets[i].transform.TransformDirection(Vector3.forward), out hit, 5) && hit.transform.tag == "Player")
+				{
+					AlertCamera(true);
+					Debug.Log("Did Hit");
+					alert.Play();
+					alarm.Play();
+				}
+				else
+				{
+					// Debug.Log("Did not Hit");
+				}
+			}
+		} else {
+			head.transform.LookAt(player.transform.position);
+		}
+	}
+
+	public void AlertCamera(bool isAlerted) {
+		alerted = isAlerted;
+	}
+}
